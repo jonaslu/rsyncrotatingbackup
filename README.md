87e137ba3b160d816d33fda76355401ec16c00aa
Jonas Lundberg <jonas.h.lundberg@gmail.com>
2013-06-03 21:47:07 +0200

    Adding script and edited readme.md

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-rsyncrotatingbackup
-===================
+# Short description
+Its a script that does rotational backups on your folders of choice. No compression involved - the files are mirrored onto the backup machine and hard links keeps redundancy down between snapshots. Done in perl via ssh and rsync (sshd  needs to be installed on the backup machine, ssh keys are optional but very handy if you automate it with cron / anacron).
 
-Perl script that does rotational backups via rsync and ssh
+For more background and running it via anacron - check the accompanying longer blog post [http://www.catchmecode.com/blog/2013/06/01/rotating-backups-with-rsync/](http://www.catchmecode.com/blog/2013/06/01/rotating-backups-with-rsync/)
+
+# Script config
+There are a couple of variables you should change - they're marked with a huge # Config part - edit these header at the top of the script.
+```perl
+# SSH user@host
+my $login = 'user@server';
+```
+SSH user and remote backup host (as you would specify it in an ssh command line).
+
+```perl
+# How many backups to keep. If you run the script once a day, 7 = 7 days of backups
+my $backupsPerCycle = 7;
+```
+Number of backups in a cycle. Will create 7 folders / snapshots - a weeks worth of backups if you run it once every day.
+
+```perl
+# Full base path on the backup host where the backups are kept - defaults to
+my $backupRootDir = "/home/user/backup/".$hostname."/backup_";
+```
+I've configured the backups to use the hostname of the backed up machine as the root folder. This way its easy to see where from what machine it came and then navigate from there as if it were on the local machine.
+
+```perl
+# Where the list of folders to back up are kept - defaults to ~/.backup_targets
+my $backupFolderList = "$ENV{HOME}/.backup_targets";
+```
+The folders to back up are configured via a file called `.backup_targets` in your home folder. The file contains folders you want backed up, separated by newline. Whitespace is ignored in the file. Example:
+```
+/home/user/Desktop
+/home/user/Pictures/
+```
+Will back up all files recursively in the home Desktop and Pictures folder. The reason the full path is given is that its mirrored with its full path on the backup machine. So these folders above are found under:
+```
+/home/storage/backup/user/client/backup_1/home/user/Desktop
+/home/storage/backup/user/client/backup_1/home/user/Pictures
+```
+#Optional config
+You might have to change these, they'll try to find the executable by themselves (via [which](http://linux.die.net/man/1/which)) but if they don't succeed - feed them the location of the binary:
+```perl
+my $rsyncCmd = `which rsync`;
+my $ssh = `which ssh`;
+```
---
+++ rsync_rotating_backups.pl
@@ -0,0 +1,89 @@
+###################################################################################
+# License: GPU General Public License. 
+# License details here: http://www.gnu.org/licenses/gpl.html
+#
+# More info and background here: 
+# http://www.catchmecode.com/blog/2013/06/01/rotating-backups-with-rsync/
+###################################################################################
+use warnings;
+use strict;
+
+my $rsyncCmd = `which rsync`;
+my $ssh = `which ssh`;
+chomp $rsyncCmd; chomp $ssh;
+
+my $hostnameCmd = `which hostname`;
+my $hostname = `$hostnameCmd`;
+chomp $hostname;
+
+###################################################################################
+# Config part - edit these
+###################################################################################
+
+# SSH user@host
+my $login = 'user@server';
+
+# How many backups to keep. If you run the script once a day, 7 = 7 days of backups
+my $backupsPerCycle = 7;
+
+# Full base path on the backup host where the backups are kept - defaults to
+my $backupRootDir = "/home/user/backup/".$hostname."/backup_";
+
+# Where the list of folders to back up are kept - defaults to ~/.backup_targets
+my $backupFolderList = "$ENV{HOME}/.backup_targets";
+
+###################################################################################
+# End config part
+###################################################################################
+
+open my $file, $backupFolderList or die "Cannot read folder list for backup, tried path is $backupFolderList";
+my @backupFolderListContents = <$file>;
+close $file;
+
+moveOldBackups();
+
+for my $backupFolder (@backupFolderListContents) {
+    chomp $backupFolder;
+    next unless $backupFolder;
+    backupDir( $backupFolder );    
+}
+
+sub moveOldBackups {
+    my @commands;
+
+    print "rotating old dirs\n";
+
+    my $removeOldestDirIfExists = "if [ -d $backupRootDir$backupsPerCycle ]; then rm -rf $backupRootDir$backupsPerCycle; fi";
+    push @commands, $removeOldestDirIfExists;
+
+    foreach my $i (reverse (1..$backupsPerCycle-1)) {
+        my $p = $i + 1;
+
+        my $moveDirsOneLevelUpIfExists = "if [ -d $backupRootDir$i ]; then mv $backupRootDir$i $backupRootDir$p; fi";
+        push @commands, $moveDirsOneLevelUpIfExists;
+    }
+    push @commands, "mkdir -p ${backupRootDir}1";
+
+    my $cmd = join(';',@commands);
+
+    print("$ssh $login \"$cmd\" 2>&1");
+}
+
+#  Copies the local dir and preserves the folder structure onto the mirroring host
+sub backupDir {
+    my $path = shift;
+    my @commands;
+
+    print "running rsync on $path\n";
+
+    my $current_dir = "${backupRootDir}2";
+    my $newest_dir = "${backupRootDir}1";
+
+    # Remove any first slash on the destination
+    (my $dest_path = $path) =~ s/^\///;
+
+    my $rsyncCmdLine = "$ssh $login \"[ -d $current_dir ]\" && $rsyncCmd -e ssh -avHzrR --delete --link-dest=\"$current_dir\" $path $login:$newest_dir ||";
+    $rsyncCmdLine .= " $rsyncCmd -e ssh -avHzrR --delete $path $login:$newest_dir";
+
+    system($rsyncCmdLine)
+}
